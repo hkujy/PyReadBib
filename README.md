1e2e19a39b1a911a172432e8d1f4b52339faec68
hkujy <phdjiangyu@outlook.com>
2021-11-14 20:16:29 +0100

    ini

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PyReadBib
+## Notes on the process 
+1. The data was extracted from Mendelay
+2. The format of the origin data in Mendelay needs to be double checked and adjust
+3. pay attention to avoid missing data entry, such as year, title and abstract.   
---
+++ StochasticAssignment/ReadBib.py
@@ -0,0 +1,37 @@
+# https://bibtexparser.readthedocs.io/en/v0.6.2/tutorial.html#preparing-a-bibtex-file
+import bibtexparser
+# remark
+# 1. the data exported should be cleaned and checked first
+
+
+with open('bibtex.bib','r',encoding='utf-8') as bibtex_file:
+    bibtex_str = bibtex_file.read()
+
+bib_database = bibtexparser.loads(bibtex_str)
+# print(bib_database.entries[1]['abstract'])
+# print(bib_database.entries[1]['title'])
+# print(bib_database.entries[1]['year'])
+# print(bib_database.entries[1]['author'])
+# print(bib_database.entries[1]['keywords'])
+
+earlist  = 2000
+for i in range(0, len(bib_database.entries)):
+    print(i)
+    if int(bib_database.entries[i]['year']) < int(earlist):
+        earlist = int(bib_database.entries[i]['year'])
+print("the earliest publication is {0}".format(earlist))
+
+abstract = []
+year = []
+title = []
+year = 1971
+with open("abstract.md","w+") as f:
+    for i in range(0, len(bib_database.entries)):
+        print("## {0}".format(bib_database.entries[i]['title']),file=f)
+        print("## {0}".format(bib_database.entries[i]['title']))
+        print("### {0}".format(bib_database.entries[i]['year']),file=f)
+        print("### {0}".format(bib_database.entries[i]['year']))
+        print("> {0}".format(bib_database.entries[i]['abstract']))
+        print("> {0}".format(bib_database.entries[i]['abstract']),file=f)
+
+
