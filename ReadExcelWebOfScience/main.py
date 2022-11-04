1ee9bdd8d9290d1f653e9b63c8858eeae3ebda45
hkujy <phdjiangyu@outlook.com>
2022-11-05 00:27:49 +0100

    IniOpen

--- ReadExcelWebOfScience/main.py
+++ ReadExcelWebOfScience/main.py
@@ -1,29 +1,12 @@
 import pandas as pd
 import re
 
-"""Remark: the excel file was checked, so the number should be less the origin excel
-"""
-
-
 def findWholeWord(w):
 	# https://cloud.tencent.com/developer/ask/45187
 	return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
 
-
 df = pd.read_excel("records.xls")
 
-
-# tt = df['Article Title']
-abs = df["Abstract"]
-# print(df['Abstract'][0])
-if findWholeWord("sustainable")(df["Abstract"][0]):
-	print("find sustainable")
-if findWholeWord("nable")(df["Abstract"][0]):
-	print("find able")
-# if "sustainable" in df["Abstract"][0]:
-#     print("find word")
-
-# ------------------------------------------
 # the following part print the abstract
 with open("PrintAbstracts.md", "w+") as f:
 	for i in range(0, len(df["Abstract"])):
@@ -31,18 +14,8 @@ with open("PrintAbstracts.md", "w+") as f:
 		print("1. "+"{0}".format(df['Author Full Names'][i]), file=f)
 		print("2. "+"{0}".format(df['Journal Abbreviation'][i]), file=f)
 		print("> "+"{0}".format(df['Abstract'][i]), file=f)
-# ------------------------------------------
-# check number of variaional inequality
-# numOfVI = 0
-# for i in range(0,len(df["Abstract"])):
-#     if (len(str(df["Abstract"][i])) >10): # remark: one record abstract is na
-#         if findWholeWord("vi")(df["Abstract"][i]):
-#             numOfVI = numOfVI + 1
-#         elif findWholeWord("variationl inequality")(df["Abstract"][i]):
-#             numOfVI = numOfVI + 1
-# print("Num of Vi = {0}".format(numOfVI))
 
-
+# the following find out th number of specific keywords
 numOfAssignment = 0
 with open("assignment.md", "w+") as f:
 	for i in range(0, len(df["Author Keywords"])):
@@ -55,6 +28,5 @@ with open("assignment.md", "w+") as f:
 				print(
 					"2. "+"{0}".format(df['Journal Abbreviation'][i]), file=f)
 				print("> "+"{0}".format(df['Abstract'][i]), file=f)
-		# elif findWholeWord("variationl inequality")(df["Author Keywords"][i]):
-		#     numOfAssignment = numOfAssignment + 1
+
 print("Num of Assignment = {0}".format(numOfAssignment))
